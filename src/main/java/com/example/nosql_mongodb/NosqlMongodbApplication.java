7c0f8e6f4f3013cf0ee4f273f28283e1e53212da
Jaeyu Jo <ehay@naver.com>
2018-10-12 21:18:14 +0900

    Init project
    add dependencies
    add config for mongodb in application.properties

---
+++ src/main/java/com/example/nosql_mongodb/NosqlMongodbApplication.java
@@ -0,0 +1,12 @@
+package com.example.nosql_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NosqlMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NosqlMongodbApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/nosql_mongodb/NosqlMongodbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.nosql_mongodb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NosqlMongodbApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
